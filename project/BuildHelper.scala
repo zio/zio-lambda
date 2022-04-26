f5c1d1ab181ace030a2db0fd21491d6f5ff8579d
Eli Kasik <soujiro32167@gmail.com>
2022-04-26 14:20:01 -0400

    #100: Cross compile to Scala 3 (#101)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,61 +6,8 @@ import BuildInfoKeys._
 object BuildHelper {
   private val Scala212             = "2.12.14"
   private val Scala213             = "2.13.8"
-  private val SilencerVersion      = "1.7.8"
-  private val KindProjectorVersion = "0.10.3"
-
-  private val stdOptions = Seq(
-    "-encoding",
-    "UTF-8",
-    "-explaintypes",
-    "-Yrangepos",
-    "-feature",
-    "-language:higherKinds",
-    "-language:existentials",
-    "-Xlint:_,-type-parameter-shadow",
-    "-Xsource:2.13",
-    "-Ywarn-numeric-widen",
-    "-Ywarn-value-discard",
-    "-unchecked",
-    "-deprecation",
-    "-Xfatal-warnings"
-  )
-
-  private val stdOpts213 = Seq(
-    "-Wunused:imports",
-    "-Wvalue-discard",
-    "-Wunused:patvars",
-    "-Wunused:privates",
-    "-Wvalue-discard"
-  )
-
-  private val stdOptsUpto212 = Seq(
-    "-Xfuture",
-    "-Ypartial-unification",
-    "-Ywarn-nullary-override",
-    "-Yno-adapted-args",
-    "-Ywarn-infer-any",
-    "-Ywarn-inaccessible",
-    "-Ywarn-nullary-unit",
-    "-Ywarn-unused-import"
-  )
-
-  private def extraOptions(scalaVersion: String) =
-    CrossVersion.partialVersion(scalaVersion) match {
-      case Some((2, 13)) =>
-        stdOpts213
-      case Some((2, 12)) =>
-        Seq(
-          "-opt-warnings",
-          "-Ywarn-extra-implicit",
-          "-Ywarn-unused:_,imports",
-          "-Ywarn-unused:imports",
-          "-opt:l:inline",
-          "-opt-inline-from:<source>"
-        ) ++ stdOptsUpto212
-      case _ =>
-        Seq("-Xexperimental") ++ stdOptsUpto212
-    }
+  private val Scala3               = "3.1.2"
+  private val KindProjectorVersion = "0.13.2"
 
   def buildInfoSettings(packageName: String) =
     Seq(
@@ -69,18 +16,20 @@ object BuildHelper {
       buildInfoObject := "BuildInfo"
     )
 
+  def kindProjector(scalaVer: String) =
+    if (scalaVer != Scala3)
+      Seq(
+        compilerPlugin("org.typelevel" %% "kind-projector" % KindProjectorVersion cross CrossVersion.full)
+      )
+    else
+      Seq.empty
+
   def stdSettings(prjName: String) =
     Seq(
       name := s"$prjName",
-      crossScalaVersions := Seq(Scala212, Scala213),
+      crossScalaVersions := Seq(Scala212, Scala213, Scala3),
       ThisBuild / scalaVersion := Scala213,
-      scalacOptions := stdOptions ++ extraOptions(scalaVersion.value),
-      libraryDependencies ++=
-        Seq(
-          ("com.github.ghik"                % "silencer-lib"    % SilencerVersion % Provided).cross(CrossVersion.full),
-          compilerPlugin(("com.github.ghik" % "silencer-plugin" % SilencerVersion).cross(CrossVersion.full)),
-          compilerPlugin("org.typelevel" %% "kind-projector" % KindProjectorVersion)
-        ),
+      libraryDependencies ++= kindProjector(scalaVersion.value),
       incOptions ~= (_.withLogRecompileOnMacro(false))
     )
 }
