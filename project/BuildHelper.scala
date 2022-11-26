45d1efc67bc266b726f2d1edd91efb99b4eafacb
Scala Steward <scala_steward@virtuslab.com>
2022-11-26 21:25:47 +0000

    Update scala3-library to 3.2.1

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import BuildInfoKeys._
 object BuildHelper {
   private val Scala212             = "2.12.16"
   private val Scala213             = "2.13.8"
-  private val Scala3               = "3.2.0"
+  private val Scala3               = "3.2.1"
   private val KindProjectorVersion = "0.13.2"
 
   def buildInfoSettings(packageName: String) =
