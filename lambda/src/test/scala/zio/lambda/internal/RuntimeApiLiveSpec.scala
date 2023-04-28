2ea8c850adba47f84fb2df030796e279dbb8b438
bogdan.vakulenko <bogdan.vakulenko@gmail.com>
2023-04-28 23:02:10 +0400

    Allows to use non-UTF-8 characters in responses.

---
+++ lambda/src/test/scala/zio/lambda/internal/RuntimeApiLiveSpec.scala
@@ -0,0 +1,30 @@
+package zio.lambda.internal
+
+import zio._
+import zio.test.Assertion._
+import zio.test._
+
+import java.net.ServerSocket
+
+object RuntimeApiLiveSpec extends ZIOSpecDefault {
+
+  override def spec =
+    suite("RuntimeApiLiveSpec spec")(
+      test("sendInvocationResponse should not throw any error when UTF-8 characters are provided") {
+
+        val env     = LambdaEnvironment("localhost:8085", "", "", 0, "", "", "", "")
+        val resp    = InvocationResponse("id", "payload with UTF-8 characters: 'тест'")
+        val runtime = new RuntimeApiLive(env)
+        for {
+          _ <- ZIO.attempt {
+                 val serverSocket = new ServerSocket(8085)
+                 val clientSocket = serverSocket.accept()
+                 clientSocket.close()
+               }.fork
+          res <- runtime.sendInvocationResponse(resp)
+        } yield assert(res)(isUnit)
+
+      }
+    )
+
+}
