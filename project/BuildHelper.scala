734774df037e70f756799691c2d75d6d69bab929
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-04-29 22:28:26 -0700

    Update scala3 to 3.3.5

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -6,7 +6,7 @@ import BuildInfoKeys._
 object BuildHelper {
   private val Scala212             = "2.12.19"
   private val Scala213             = "2.13.16"
-  private val Scala3               = "3.3.3"
+  private val Scala3               = "3.3.5"
   private val KindProjectorVersion = "0.13.3"
 
   def buildInfoSettings(packageName: String) =
