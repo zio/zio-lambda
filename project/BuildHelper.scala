8a7c709a0b0fdf6d4db86c5cad894f106c9b8832
Adam Hearn <22334119+hearnadam@users.noreply.github.com>
2025-12-29 16:37:39 -0800

    Remove unused sbt plugins (#399)

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -4,10 +4,9 @@ import sbtbuildinfo._
 import BuildInfoKeys._
 
 object BuildHelper {
-  private val Scala212             = "2.12.19"
-  private val Scala213             = "2.13.16"
-  private val Scala3               = "3.3.5"
-  private val KindProjectorVersion = "0.13.3"
+  private val Scala212 = "2.12.19"
+  private val Scala213 = "2.13.16"
+  private val Scala3   = "3.3.5"
 
   def buildInfoSettings(packageName: String) =
     Seq(
@@ -16,20 +15,10 @@ object BuildHelper {
       buildInfoObject := "BuildInfo"
     )
 
-  def kindProjector(scalaVer: String) =
-    if (scalaVer != Scala3)
-      Seq(
-        compilerPlugin("org.typelevel" %% "kind-projector" % KindProjectorVersion cross CrossVersion.full)
-      )
-    else
-      Seq.empty
-
   def stdSettings(prjName: String) =
     Seq(
       name := s"$prjName",
       crossScalaVersions := Seq(Scala212, Scala213, Scala3),
-      ThisBuild / scalaVersion := Scala213,
-      libraryDependencies ++= kindProjector(scalaVersion.value),
-      incOptions ~= (_.withLogRecompileOnMacro(false))
+      ThisBuild / scalaVersion := Scala213
     )
 }
